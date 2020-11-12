72132c6d000c23eea64c50410bdcce98ab941e03
Amreiz <amreizsuma@gmail.com>
2020-11-12 13:18:55 +0000

    Main menu basic layout
    Created the buttons behind the main menu. Fully set up for functionality by the next commit.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayExercise()
+    {
+        SceneManager.LoadScene("Exercise_01"); // Loads the first exercise
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
