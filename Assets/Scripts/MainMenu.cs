6fea5c19423fd1d9876362591e73e10ddf75b441
Amreiz <amreizsuma@gmail.com>
2020-11-22 12:21:11 +0000

    Bug on build. Needs investigating
    A build error with a missing reference exception, GUI error pushing more GUICLips than you are popping, Error building player

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.Management;
 
 public class MainMenu : MonoBehaviour
 {
+    //ManualXRControl man = new ManualXRControl();
+
+    //void Start()
+    //{
+    //    man.StartXR();
+    //}
+
     public void PlayExercise()
     {
-        SceneManager.LoadScene("Exercise_01"); // Loads the first exercise
+        //XRGeneralSettings.Instance.Manager.InitializeLoader();
+        //XRGeneralSettings.Instance.Manager.StartSubsystems();
+
+        SceneManager.LoadScene("HelloCardboard"); // Loads the first exercise
     }
 
     public void QuitGame()
